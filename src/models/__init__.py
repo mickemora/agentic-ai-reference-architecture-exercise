6716af4a8997b66210027f745a9408a4bc5171e9
Mike Mora <mike@mikemora.com>
2026-09-12 18:40:20 -0500

    v1: add src/models/__init__.py

---
+++ src/models/__init__.py
@@ -0,0 +1,5 @@
+from src.models.claim import FailureCause, WarrantyClaim
+from src.models.decision import CoverageDecision, Decision, ReasonCode
+from src.models.vehicle import VehicleDetails
+
+__all__ = ["CoverageDecision", "Decision", "FailureCause", "ReasonCode", "VehicleDetails", "WarrantyClaim"]
